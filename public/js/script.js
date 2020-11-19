a2fcfdfea0a92ad78acc6a17d34a9d3352176540
hentschellj <hentschellj@gmail.com>
2020-11-19 15:11:37 -0600

    Added basic click event for operator btns

--- public/js/script.js
+++ public/js/script.js
@@ -3,5 +3,9 @@
 
   $('#calc .digit').click(function() {
     console.log('Digit clicked');
+  });
+
+  $('#calc .operator-group .btn').click(function() {
+    console.log('Operator clicked');
   })
 })();
